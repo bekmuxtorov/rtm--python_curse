5b5a76bf208dcb0d43b9bd69cb209353d31ca9a6
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2022-06-24 22:42:37 +0500

    rtmdars1

---
+++ rtmdars1.py
@@ -0,0 +1,121 @@
+"""============================== 1-misol =============================="""
+# print('Asadbek')
+# print("""Muxtorov""")
+
+"""============================== 2-misol =============================="""
+# print('''Asadbek''')
+# print("Muxtorov")
+# print("Abdurasul o'g'li")
+# print("Quva tumani, Gulobod MFY, Fayzlik ko'chasi 111 uy")
+
+"""============================== 3-misol =============================="""
+# print('''1)Kel Nyuport "Diqqat"''')
+# print('''2)Jorj Samuel Kleyson "Vavilionlik eng boy odam"''')
+# print('''3)Ahmad Lutfiy Qozonchi "Saodat asri qissalari"''')
+
+"""============================== 4-misol =============================="""
+# heigth = 3  # To'g'ri to'rtburchak bo'yi
+# width = 4   # To'g'ri to'rtburchak eni
+
+# area = heigth * width   # To'g'ri to'rtburchak yuzi S = a * b 
+# print("To'gri to'rtburchak yuzi S =", area)
+
+"""============================== 5-misol =============================="""
+# heigth = 3   # To'g'ri to'rtburchak bo'yi
+# width = 4    # To'g'ri to'rtburchak eni
+
+# perimetr = 2 * (heigth + width)  # To'g'ri to'rtburchak perimentri P = 2 * (a + b)
+# print("To'g'ri to'rtburchak perimetri P=",perimetr)
+
+"""============================== 6-misol =============================="""
+# num1 = 5
+# num2 = 8 
+# num3 = 6
+
+# sum = num1 + num2 + num3
+# print(sum)
+
+"""============================== 7-misol =============================="""
+# ball1 = 3
+# ball2 = 4
+# ball3 = 5
+# ball4 = 3
+
+# avarage = (ball1 + ball2 + ball3 + ball4) / 4
+# print(avarage)
+
+"""============================== 8-misol =============================="""
+# oraliq1 = 4
+# oraliq2 = 5
+# oraliq3 = 3
+# oraliq4 = 2
+# ortacha = (oraliq1 + oraliq2 + oraliq3 + oraliq4) // 5
+# print(ortacha)
+
+"""============================== 9-misol =============================="""
+# name = input('Ismingizni kiriting: ')
+
+# print("Yashash manzilizingizni kiriting:")
+# shahar = input('Shaharni kiriting: ')
+# tuman = input('Tumaningizni kiriting: ')
+# pochta = input('Pochta indeksini kiriting: ')
+
+# phone_number = input('Telefon raqamingizni  kiriting: ')
+
+# kasb = input('Kasbingizni kiriting: ')
+
+# print(f"""Sizning ma'lumotlaringiz:
+#       Ism: {name}
+#       Shahar: {shahar}
+#       Tuman: {tuman}
+#       Pochta Index: {pochta}
+#       Telefon raqam: {phone_number}
+#       Kasb: {kasb}
+#       """)
+
+"""============================== 10-misol =============================="""
+# speed = int(input('Avtomashina tezligini kiriting: '))
+
+# distance1 = speed * 6
+# distance2 = speed * 10
+# distance3 = speed * 15
+
+# print(f"6 soatda bosib o'tilgan yo'l: S1 = {distance1}")
+# print(f"10 soatda bosib o'tilgan yo'l: S2 = {distance2}")
+# print(f"15 soatda bosib o'tilgan yo'l: S3 = {distance3}")
+
+"""============================== 11-misol =============================="""
+# distance = int(input('Bosib o\'tilgan yo\'ni kiriting: S= '))
+# fuel = int(input('Sarf qilingan yoqilg\'ini kiriting: L= '))
+
+# # S km ---> L litr
+# # 1 km ---> X litr
+# # X = L * 1 / S
+
+# unity_fuel = fuel  / distance #unity_fuel ---> birlik yoqilg'i
+# print(unity_fuel)
+
+"""============================== 12-misol =============================="""
+# salary = int(input("Ish haqingizni kiriting: "))   # ish haqi
+# cost = int(input("Oylik xarajatni kiriting: "))    # oylik xarajat
+
+# net_profit = salary - cost # Oylik sof daramad
+# print(f"Oylik sof daromadingiz: {net_profit}")
+
+"""============================== 13-misol =============================="""
+# num = input('Sonni kiriting: ')
+# print(num * 2)
+
+"""============================== 14-misol =============================="""
+# cubic_side = int(input('Kubning tomonini kiriting: '))   #kubning tomonini kiriting
+# size = cubic_side**3   # Kubning xajmi
+# print(size)
+
+"""============================== 15-misol =============================="""
+# ball1 = int(input('Birinchi baho: '))
+# ball2 = int(input('Ikkinchi baho: '))
+# ball3 = int(input('Uchinchi baho: '))
+# ball4 = int(input('To\'inchi baho: '))
+
+# avarage = (ball1 + ball2 + ball3 + ball4) / 4
+# print(avarage)
