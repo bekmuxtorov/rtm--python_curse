cfd2d09fcfd3f67ff8b78c5807e4d9ab4e2dda9d
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2022-06-29 22:33:57 +0500

    rtmdars3.py

---
+++ rtmdars3.py
@@ -0,0 +1,128 @@
+"""============================== if 1 =============================="""
+# num = int(input("Sonni kiriting: "))
+# if num > 0:
+#     num += 1
+
+# print(num)
+
+"""============================== if 2 =============================="""
+# num = int(input("Sonni kiriting: "))
+# if num > 0:
+#     num += 1
+# else:
+#     num -= 2
+
+# print(num)
+
+"""============================== if 3 =============================="""
+# num = int(input("Sonni kiriting: "))
+# if num > 0:
+#     num += 1
+# elif num < 0:
+#     num -= 2
+# else:
+#     num = 10
+    
+# print(num)
+
+"""============================== if 4 =============================="""
+# count = 0
+# for i in range(3):
+#     num = int(input(f"{i+1}-sonni kiriting: "))
+#     if num > 0:
+#         count += 1
+        
+# print(f"Musbat butun sonlar soni: {count}")
+
+"""============================== if 5 =============================="""
+# count_positive = 0
+# count_negative = 0
+# for i in range(3):
+#     num = int(input(f"{i+1}-sonini kiriting: "))
+#     if num > 0:
+#         count_positive += 1
+#     else:
+#         count_negative += 1
+
+# print(f"Musbat butun sonlarning soni: {count_positive}")
+# print(f"Manfiy butun sonlarning soni: {count_negative}") 
+
+"""============================== if 6 =============================="""       
+# num1 = int(input("a sonni kiriting: a= "))
+# num2 = int(input("b sonni kiriting: b= "))
+# result = 0
+
+# if num1 > num2:
+#     result = num1
+# else:
+#     result = num2
+
+# print(f"{num1} va {num2} sonlarning kattasi: {result}")
+    
+"""============================== if 7 =============================="""
+# num1 = int(input("a sonni kiriting: "))
+# num2 = int(input("b sonni kiriting: "))
+
+# if num1 > num2:
+#     result = 1
+# else:
+#     result = 2
+# print(f"{num1} va {num2} sonlarning kattasini tartib raqami: {result}")
+
+"""============================== if 8 =============================="""
+# num1 = int(input("a sonni kiriting: "))
+# num2 = int(input("b sonni kiriting: "))
+
+# if num1 > num2:
+#     print(num1,num2)
+# else:
+#     print(num2,num1)
+
+"""============================== if 9 =============================="""
+# A = int(input("A sonini kiriting: "))
+# B = int(input("B sonini kiriting: "))
+
+# if A < B:
+#     print(f"A = {A}, B = {B}")
+# else:
+#     print(f"A = {B}, B = {A}")
+    
+"""============================== if 10 =============================="""
+# A = int(input("A sonini kiriting: "))
+# B = int(input("B sonini kiriting: "))
+
+# if A != B:
+#     A = A + B
+#     B = A
+# elif A == B:
+#     A = 0
+#     B = A
+# print(f"A = {A}, B = {B}")
+    
+
+        
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
