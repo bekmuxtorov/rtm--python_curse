01596f0ae1466110e716ec2285283570ce0bc299
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2022-07-01 18:39:23 +0500

    rtm lesson 4

---
+++ rtmdars4.py
@@ -0,0 +1,106 @@
+"""============================== 1 - masala =============================="""  
+# yosh = int(input("Yoshingizni kiriting: "))
+# if 16 < yosh and yosh < 60:
+#     result = "Siz ishlashingiz mumkin!"
+# else:
+#     result = "Siz ishlashingiz mumkin emas!"
+
+# print(result)
+
+"""============================== 2 - masala =============================="""  
+# ball = int(input("To'plagan balingizni kiriting: "))
+# if 130 < ball and ball < 180:
+#     result = "Siz shartnoma asosida o'qishga qabul qilindingiz!"
+# else:
+#     result = "Siz shartnoma asosida o'qishga qabul qilinmadingiz!"
+
+# print(result)
+
+"""============================== 3 - masala =============================="""  
+# print("Bo'y uzunligini kiriting: ")
+# uzunlik = int(input(">>>"))
+# print("Massani kiriting: ")
+# massa = int(input(">>>"))
+
+# if uzunlik > 170 and massa > 65:
+#     result = "Siz harbiy xizmatga borishingiz mumkin "
+# else:
+#     result = "Siz harbiy xizmatga borishingiz mumkin emas!"
+    
+# print(result)
+
+"""============================== 4 - masala =============================="""  
+# num1 = int(input("Birinchi sonini kiriting: "))
+# num2 = int(input("Ikkinchi sonini kiriting: "))
+# num3 = int(input("Uchinchi sonini kiriting: "))
+
+# if num1 > num2:
+#     if num1 > num3:
+#         result = num1
+#     else:
+#         result = num3
+        
+# else:
+#     if num2 > num3:
+#         result = num2
+#     else:
+#         result = num3
+# print(f"{num1}, {num2}, {num3} lardan kattasi: {result}")
+        
+"""============================== 5 - masala =============================="""  
+# num1 = int(input("Birinchi sonini kiriting: "))
+# num2 = int(input("Ikkinchi sonini kiriting: "))
+# num3 = int(input("Uchinchi sonini kiriting: "))
+
+# if num1 < num2:
+#     if num1 < num3:
+#         result = num1
+#     else:
+#         result = num3
+       
+# else:
+#     if num2 < num3:
+#         result = num2
+#     else:
+#         result = num3
+# print(f"{num1}, {num2}, {num3} lardan kichigi: {result}")   
+    
+"""============================== 6 - masala =============================="""  
+# ball1 = int(input("Birinchi bahoni kiriting: "))
+# ball2 = int(input("Ikkinchi bahoni kiriting: ")) 
+
+# if ball1 > 4 or ball2 > 4:
+#     result = "Siz imtihondan o'tdingiz!"
+# else:
+#     result = "siz imtihondan o'ta olmadingiz!"
+    
+# print(result)
+
+"""============================== 7 - masala =============================="""  
+# sum_narx = int(input("Umumiy summani kiriting: "))
+# sum_son = int(input("Mahsulotlar sonini kiriting: "))
+
+# if sum_narx > 200000 and sum_son > 5:
+#     result = "Siz 20% chegirmaga ega bo'ldingiz!"
+    
+# else:
+#     result = "Haridingiz uchun raxmat. Kelib turing :)"
+    
+# print(result)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
