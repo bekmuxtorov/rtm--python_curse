dcb6a8940c1f5fdd27f4efea1551462f8f3de414
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2022-07-02 22:10:55 +0500

    rtm-python 5-lesson

---
+++ rtmdars5.py
@@ -0,0 +1,66 @@
+"""
+For bilan ishlash
+"""
+
+"""========================================== for 1 =========================================="""
+# k = int(input("k sonini kiriting: "))
+# n = int(input("n sonini kiriting: "))
+# if n > 0:
+#     for i in range(n):
+#         print(k)
+
+# else:
+#     print('Kiritishda xatolik!!!')
+    
+"""========================================== for 2 =========================================="""
+# a = int(input("a sonini kiriting: "))
+# b = int(input("b sonini kiriting: "))
+
+# if a < b:
+#     for i in range(a, b + 1):
+#         print(i)
+#     print(f"Ekranga chiqarilgan sonlar sonini: {b + 1 - a}")   
+
+# else:
+#     print('Kiritishda xatolik!!')
+
+"""========================================== for 3 =========================================="""
+# a = int(input("a sonini kiriting: "))
+# b = int(input("b sonini kiriting: "))
+
+# if a < b:
+#     for i in range(b-1, a, -1):
+#         print(i)
+#     print(f"Ekranga chiqarilgan sonlar sonini: {b - 1 - a}")   
+
+# else:
+#     print('Kiritishda xatolik!!')
+
+"""========================================== for 4 =========================================="""
+# single_price = int(input("Bir kg konfet narxi: "))
+# for i in range(1,11):
+#     print(i * single_price)
+
+"""========================================== for 5 =========================================="""
+# single_price = int(input("Bir kg konfet narxi: "))
+
+# start = 0.1
+# end = 0.9
+# while start < end:
+#     print(start * single_price)
+#     start += 0.1
+
+"""========================================== for 6 =========================================="""
+single_price = int(input("Bir kg konfet narxi: "))
+
+start = 1.2
+end = 2
+while start < end:
+    print(start * single_price)
+    start += 0.2
+
+
+
+    
+    
+    
