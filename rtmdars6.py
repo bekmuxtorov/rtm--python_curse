6659a962a558607690ac740093f19c0bac186c7f
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2022-07-03 21:54:06 +0500

    rtm-python 6-lesson

---
+++ rtmdars6.py
@@ -0,0 +1,126 @@
+"""
+Ro'yhat ga oid masalalar
+"""
+"""============================== 1 - masala =============================="""
+# students = ['Asadbek','Abdunabi','Alisher']
+# new_student = input("Yangi talaba ismini kiriting: ")
+# students.append(new_student)
+# print(students)
+
+"""============================== 2 - masala =============================="""
+# students = ['Asadbek','Abdunabi','Alisher']
+# new_student = input("Talabani ismini kiriting: ")
+# students.insert(2,new_student)
+# print(students)
+
+"""============================== 3 - masala =============================="""
+# wins = ["Asadbek","Abdunabi","Abdusalom"]
+# print(wins)
+# wins.remove(wins[len(wins) - 1 ])
+# print(wins)
+
+"""============================== 4 - masala =============================="""
+# nums = [10,8,9,6,5,4,3,7,2,1]  
+# print(nums) 
+# nums.remove(nums[4])
+# print(nums)
+
+"""============================== 5 - masala =============================="""
+# nums = [10,8,9,6,5,4,3,7,2,1]
+# print(nums)
+# n = int(input("O'chirilishi kerak bo'lgan elament indeksini kiriting: "))
+# if n < len(nums):
+#     nums.remove(nums[n-1])
+# else:
+#     print("Kiritishda xartolik!!! ")
+# print(nums)
+
+"""============================== 6 - masala =============================="""
+# nums = [10,8,9,6,5,4,3,7,2,1]  
+# print(nums)
+# for j in range(len(nums) - 1):
+#     for i in range(len(nums) - 1):
+#         if nums[i] > nums[i+1]:
+#             nums[i], nums[i+1] = nums[i+1], nums[i]
+        
+# print(nums)
+
+"""============================== 7 - masala =============================="""
+# nums = [10,8,9,6,5,4,3,7,2,1]  
+# print(nums)
+# for j in range(len(nums) -1):
+#     for i in range(len(nums) - 1):
+#         if nums[i] > nums[i+1]:
+#             nums[i], nums[i+1] = nums[i+1], nums[i]
+            
+# nums.reverse()
+# print(nums)
+
+"""============================== 8 - masala =============================="""
+# nums = [10,8,9,6,5,4,3,7,2,1]  
+# print(f"5 sonini {nums.count(5)} marta ishtirok etgan.") 
+
+"""============================== 9 - masala =============================="""
+# friends = ["Anvar","Bobur","Sardor","Sarvar","Muhammad"]
+# index = friends.index("Sardor")
+# print(f"Sardorni indeks raqami: {index}") 
+
+"""============================== 10 - masala =============================="""
+# nums = [10,22,3,12,4,3,32,4]  
+# for _ in range(len(nums)):
+#     for i in range(len(nums) - 1):
+#         if nums[i] > nums[i+1]:
+#             nums[i], nums[i+1] = nums[i+1], nums[i] 
+            
+# print(f"To'plamdagi eng katta son: {nums[-1]}")
+
+"""============================== 11 - masala =============================="""
+# nums = [10,22,3,12,4,3,32,4]
+# print(min(nums)) 
+
+"""============================== 12 - masala ==============================""" 
+# nums = [10,22,3,12,4,3,32,4] 
+# summa = 0
+# for num in nums:
+#     summa += num
+# print(summa)
+
+"""============================== 13 - masala ==============================""" 
+# nums = [10,22,3,12,4,3,32,4] 
+# result = 1  
+# for num in nums:
+#     result *= num
+# print(result)
+
+"""============================== 14 - masala ==============================""" 
+# nums  = [1,2,3,4,5,6,7] 
+# print(nums[1:4])
+
+"""============================== 15 - masala ==============================""" 
+nums  = [1,2,3,4,5,6,7] 
+print(len(nums))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
