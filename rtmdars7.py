70e0ae9f2712e90df9bca86eee883309557f7e8c
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2022-07-03 21:54:47 +0500

    rtm-python 7-lesson

---
+++ rtmdars7.py
@@ -0,0 +1,90 @@
+"""
+Lug'atlar bilan ishlash
+"""
+"""============================== 1 - masala =============================="""
+# talabalar = {
+#     "talaba1":["Tolib",3],
+#     "talaba2":["Akmal",4],
+#     "talaba3":["Botir",5]
+#     }
+
+# ism = input("Talabaning ismi: ")
+# ball = int(input("Talaba olgan oraliq bahosi: "))
+# talabalar["talaba4"] = []
+# talabalar["talaba4"].append(ism)
+# talabalar["talaba4"].append(ball)
+# print(talabalar)
+
+# del talabalar["talaba1"]
+# print(talabalar)
+
+"""============================== 2 - masala =============================="""
+# en_uz = {
+#     'car':'mashina',
+#     'apple':'olma',
+#     'notebook':'daftar',
+#     'phone':'telefon',
+#     'unique':'yagona'
+#     }
+
+# en_word = input("So'z kiriting(ingliz tilidagi): ")
+
+# if en_word in en_uz.keys():
+#     uz_word = en_uz[en_word]
+#     print(f"{en_word} -- {uz_word}")
+
+# else:
+#     print(f"\"{en_word}\" lug'atda mavjud emas")
+
+"""============================== 3 - masala =============================="""
+# dic1={1:10, 2:20}
+# dic2={3:30, 4:40}
+# dic3={5:50,6:60}
+
+# dic1.update(dic2)
+# dic1.update(dic3)
+# print(dic1)
+
+"""============================== 4 - masala =============================="""
+# data = {
+#         "alisher" : 12,
+#         'ganisher' : 10,
+#         'olimsher' : 30 
+#         }
+
+## Tug'ilgan kunni qidirib toping
+# ism = input("Ismnini kiriting: ")
+# print(f"{ism}ning tug'ilgan kuni: {data[ism]}")
+
+## Yangi tug'ilgan kunni qo'shing
+# new_name = input("Ismnini kiriting: ")
+# new_day = input(f"{new_name}ning tug'ilgan kuni: ")
+# data.setdefault(new_name,new_day)
+
+## Tug'ilgan kunni o'zgartiring
+# name = input("Ismni kiriting: ")
+# day = input("O'zgartirish kerak bo'lgan kun: ")
+# data[name] = day
+
+# # Tug'ilgan kunni o'chirib tashlang
+# ism = input("Ismni kiriting: ")
+# del data[ism]
+# print(data)
+
+"""============================== 5 - masala =============================="""
+# room = {
+#         "CS101" : 3004,
+#         "CS102" : 4501,
+#         "CS103" : 6755,
+#         "NT110" : 1244,
+#         "CM241" : 1411
+#         }
+
+"""============================== 6 - masala =============================="""
+# teachers = {
+#     "CS101"	: "Haynes",
+#     "CS102"	: "Alvarado",
+#     "CS103"	: "Rich",
+#     "NT110"	: "Burke",
+#     "CM241" : "Lee"
+#     }
